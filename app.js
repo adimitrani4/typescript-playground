5f48caff2ca7efc7d50172d31a07d0ac3e81b5b1
adimitrani4 <adi.mitrani4@gmail.com>
2021-12-23 18:27:37 +0200

    fixes and gitignore

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-var port = 8080;
-app.listen(port, function () {
-    console.log("Application is running on port ".concat(port, "."));
-});
