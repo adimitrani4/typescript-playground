ccce0780b3ba23271634290ba93729c0d84ef5f7
adimitrani4 <adi.mitrani4@gmail.com>
2022-01-02 11:49:58 +0200

    docker compose

---
+++ backend/src/app.ts
@@ -0,0 +1,42 @@
+import express, {Request, Response, NextFunction } from 'express';
+import { Movie } from './movie';
+import movies_data from './server_data/movies_data.json';
+
+const app = express();
+const port = 8080;
+
+const movies : Movie[] = movies_data;
+console.log(`got ${movies.length} movies from json`);
+
+//To solve the bug: No 'Access-Control-Allow-Origin' header is present on the requested resource
+app.use((req, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+// app.get('/', (_req, res) => {
+//     res.send(`Hello ${process.env.env_user_name}!`)
+//   })
+
+// app.get('/hello', (_req, res) => {
+//     res.send(`Hello World!`)
+//   })
+
+// app.get('/helloTo/:userName', function (req, res) {
+//     res.send(`Hello ${req.params.userName}!`)
+//   })
+
+app.get('/movie/:movieName', (req: Request, res: Response) =>{
+  const selectedMovie: Movie | undefined = movies.find(movie => movie.title === req.params.movieName);
+  res.send(selectedMovie);
+});
+
+app.get('/moviesForGenre/:genre', (req: Request, res: Response) =>{
+  //if there is no movie with this gemere will return an empty array
+  const selectedMovie: Movie[]= movies.filter(movie => movie.genres.includes(req.params.genre));
+  res.send(selectedMovie);
+});
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}.`);
+});
