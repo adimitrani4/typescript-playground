389f111b9a6a474a92f7fc1d8456afcd77bc9a2e
adimitrani4 <adi.mitrani4@gmail.com>
2022-01-04 12:24:29 +0200

    Changed class to function components

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
 import Movie from './movie';
+// import {MoviesList} from './MoviesList';
 
-function MoviesList(props: { movies: Movie[]; })
+const MoviesList = (props: { movies: Movie[]; }) =>
 {
     return(
         <>{
@@ -37,65 +38,50 @@ interface FormProps
     onSubmit: (movieData: Movie[]) => void
 }
 
-interface FormState{
-    movieGenre: string
-}
+const Form = (props:FormProps) => {
+    const [movieGenreState, setMovieGenre] : [string, (genre: string) => void] = React.useState('');
 
-class Form extends React.Component<FormProps, FormState>
-{
-    state = { 
-        movieGenre: '' 
-    };
-
-	handleSubmit = async (event: { preventDefault: () => void; }) => {
-  	    event.preventDefault();
-        const resp = await axios(`http://localhost:8080/moviesForGenre/${this.state.movieGenre}`);
+    const handleSubmit = async (event: { preventDefault: () => void; }) => 
+    {
+        event.preventDefault();
+        const resp = await axios(`http://localhost:8080/moviesForGenre/${movieGenreState}`);
         console.log(resp);
-        this.props.onSubmit(resp.data);
+        props.onSubmit(resp.data);
     };
 
-	render() {
-  	return (
-    	<form onSubmit={this.handleSubmit}>
+    return (
+    	<form onSubmit={handleSubmit}>
     	  <input 
           type="text" 
-          value={this.state.movieGenre}
-          onChange={event => this.setState({ movieGenre: event.target.value })}
+          value={movieGenreState}
+          onChange={event => setMovieGenre(event.target.value)}
           placeholder="Enter Movie Genre" 
           required 
         />
         <button>Search</button>
     	</form>
     );
-  }
+
 }
 
-interface AppState {
-    movieData: Movie[]
-}
+const App = () =>{
+    const [movieDataState, setMovieData] : [Movie[], (movieData:Movie[])=>void] = React.useState([]);
 
-class App extends React.Component<{}, AppState> {
-    state = {
-        movieData: []
-    };
-
-    render() {
-      return (
+    return (
         <div className='app'>
             <div className="logo">
                 <img src="baseline_star_rate_black_24dp.png"/>
             </div>
             <div className='logic'>
             <Form onSubmit={(newMovieData: Movie[]) => {
-        this.setState({movieData: newMovieData});}
+        setMovieData(newMovieData);}
             }/>
-            <MoviesList movies={this.state.movieData}/>
+            <MoviesList movies={movieDataState}/>
             </div>
         </div>
       );
-    }
-  }
-  
+}
+
   ReactDOM.render(
     <App />,
     document.getElementById('root')
